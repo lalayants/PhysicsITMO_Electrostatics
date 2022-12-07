7c7597bf09fa40ff8a3cc9dcbfdad0b07ecacd92
Lalayants <91465467+Lalayants@users.noreply.github.com>
2022-12-07 22:24:03 +0300

    project 2.1 still not good

--- Project2.1/model.py
+++ Project2.1/model.py
@@ -3,7 +3,6 @@ import numpy as np
 
 mu0 = np.pi * 4 * 10 * -7
 
-
 def _getStartV(start_angels, v0):
     v = np.array([np.cos(start_angels[0]), np.sin(start_angels[0]), np.sin(start_angels[1])])
     return np.array(v0 * v / np.linalg.norm(v)).reshape(-1, 3)
@@ -16,15 +15,12 @@ def _getModuleB(I, point):
 def _getB(I, point):
     B = _getModuleB(I, point)  # модуль вектора В
     r = np.array([0, point[1], point[2]])  # вектор от провода до заряда
-    r = r / np.linalg.norm(r)  # нормированный вектор от провода до заряда
     vectB = np.cross([1, 0, 0], r)  # направление магнитного поля
     normVectB = vectB / np.linalg.norm(vectB)  # нормированное направление вектор B
     return normVectB * B  # направление на модуль - искомый вектор
 
 
 def _getF(q, V, I, point):
-    # print(_getB(I, point), _getB(I, point).shape) 
-    # print(V, V.shape)
     return q * np.cross(V, _getB(I, point))
 
 
@@ -33,7 +29,6 @@ def _calculate_speed(V, q, m, dt, I, point):
 
 
 def _calculate_position(point, V, dt):
-    # print(V, point)
     return point + V * dt
 
 
@@ -41,13 +36,8 @@ def calculate_trajectory(start_position, start_angels, v0, q, m, I, R, dt=0.001)
     t = 0
     V = _getStartV(start_angels, v0)
     positions = np.array(start_position).reshape(-1, 3)
-    # print(positions, V)
     while np.linalg.norm(positions[-1][1:]) > R and t < 3:
         t += dt
-        # print(V, _calculate_speed(V[-1], q, m, dt, I, positions[-1]))
         V = np.append(V, _calculate_speed(V[-1], q, m, dt, I, positions[-1]).reshape(1,3), axis=0)
-        # print(t, V[-1])
-        # print(V)
         positions = np.append(positions, _calculate_position(positions[-1], V[-1], dt).reshape(1,3), axis=0)
-
     return positions
